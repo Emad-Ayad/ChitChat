5cbcef6880f5d89f2903725596c2404a3306d14e
Emad-Ayad <emadayad228@gmail.com>
2024-12-19 18:06:30 +0200

    add register cubit

--- lib/view/screens/register_screen.dart
+++ lib/view/screens/register_screen.dart
@@ -1,8 +1,10 @@
 import 'package:chitchat/util/constant.dart';
+import 'package:chitchat/view/cubits/register_cubit/register_cubit.dart';
 import 'package:chitchat/view/widgets/custom_button.dart';
 import 'package:chitchat/view/widgets/custom_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
@@ -18,118 +20,123 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   String? password;
 
-  GlobalKey<FormState> formKey =GlobalKey();
+  GlobalKey<FormState> formKey = GlobalKey();
 
-  bool isLoading=false;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isLoading,
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Form(
-            key: formKey ,
-            child: ListView(
-              children: [
-                Image.asset(
-                  "assets/images/feather.png",
-                  height: 200,
-                  width: 200,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "ChitChat",
-                      style: GoogleFonts.satisfy(
-                        textStyle:
-                            const TextStyle(color: Colors.white, fontSize: 32),
+    return BlocProvider(
+      create: (context) => RegisterCubit(),
+      child: BlocConsumer<RegisterCubit, RegisterState>(
+        listener: (context, state) {
+          if (state is RegisterLoading) {
+            isLoading = true;
+          } else if (state is RegisterSuccess) {
+            isLoading = false;
+            Navigator.pushNamed(context, 'ChatScreen');
+          } else if (state is RegisterFailure) {
+            isLoading = false;
+            showSnackBar(context, state.errMsg);
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: isLoading,
+            child: Scaffold(
+              backgroundColor: kPrimaryColor,
+              body: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Form(
+                  key: formKey,
+                  child: ListView(
+                    children: [
+                      Image.asset(
+                        "assets/images/feather.png",
+                        height: 200,
+                        width: 200,
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 75),
-                const Row(
-                  children: [
-                    Text("Sign Up",
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: CustomTextField(
-                    hintText: "Email",
-                    onChanged: (data) {
-                      email = data;
-                    },
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "ChitChat",
+                            style: GoogleFonts.satisfy(
+                              textStyle:
+                              const TextStyle(
+                                  color: Colors.white, fontSize: 32),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 75),
+                      const Row(
+                        children: [
+                          Text("Sign Up",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20)),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: CustomTextField(
+                          hintText: "Email",
+                          onChanged: (data) {
+                            email = data;
+                          },
+                        ),
+                      ),
+                      CustomTextField(
+                        hintText: "Password",
+                        onChanged: (data) {
+                          password = data;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomButton(
+                        contentText: "Sign Up",
+                        onTap: () async {
+                          if (formKey.currentState!.validate()) {
+                            BlocProvider.of<RegisterCubit>(context)
+                                .registerUser(
+                                email: email!, password: password!);
+                          }
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Already have an Account ? ",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text("Login")),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                CustomTextField(
-                  hintText: "Password",
-                  onChanged: (data) {
-                    password = data;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                CustomButton(
-                  contentText: "Sign Up",
-                  onTap: () async {
-                    if(formKey.currentState!.validate()) {
-                      isLoading=true;
-                      setState(() {});
-                      try {
-                        await registerUser();
-                        Navigator.pushNamed(context, 'ChatScreen');
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'weak--password') {
-                          showSnackBar(context, "weak password !!");
-                        } else if (e.code == 'email-already-in-use') {
-                          showSnackBar(context, "This email is Already used !!");
-                        }
-                      }
-                      isLoading=false;
-                      setState(() {});
-                    }
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Already have an Account ? ",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text("Login")),
-                  ],
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
 
-  void showSnackBar(BuildContext context,String message) {
+  void showSnackBar(BuildContext context, String message) {
     ScaffoldMessenger.of(context).showSnackBar(
-       SnackBar(
+      SnackBar(
         content: Text(message),
       ),
     );
   }
 
-  Future<void> registerUser() async {
-    UserCredential user = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(
-            email: email!, password: password!);
-  }
+
 }
